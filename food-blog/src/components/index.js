650a39bbdfa86dd63ebaec6d810c33f00b9b76d7
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:44:12 +0200

    Add files via upload

---
+++ food-blog/src/components/index.js
@@ -0,0 +1,22 @@
+import RecipeSearch from "./RecipeSearch/RecipeSearch";
+import MainNav from "./MainNav/MainNav";
+import Recipes from "./Recipes/Recipes";
+import BlogPosts from "./BlogPosts/BlogPosts";
+import BecomeAuthor from "./BecomeAuthor/BecomeAuthor";
+import UserReviews from "./UserReviews/UserReviews";
+import Footer from "./Footer/Footer";
+import FeaturedArticles from "./FeaturedArticles/FeaturedArticles";
+import SearchContainer from "./SearchContainer/SearchContainer";
+import BlogArticle from "./BlogArticle/BlogArticle";
+export {
+  MainNav,
+  RecipeSearch,
+  Recipes,
+  BlogPosts,
+  BecomeAuthor,
+  UserReviews,
+  Footer,
+  FeaturedArticles,
+  SearchContainer,
+  BlogArticle
+};
