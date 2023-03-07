b12895e13b11dc52a0d532ce21e0183d14aa62fb
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:14:31 +0200

    Add files via upload

---
+++ project3/src/assets/index.js
@@ -0,0 +1,5 @@
+import unavailableImg from "./poster-holder.jpg";
+import unavailableLandscapeImg from "./image-not-found.jpg";
+import noPictureImg from "./no-picture.jpg";
+
+export {unavailableImg, unavailableLandscapeImg, noPictureImg}
