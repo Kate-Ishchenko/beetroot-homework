b5b7df34488a0f583a5274748f4fcab257e5bc2e
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:38:39 +0200

    Add files via upload

---
+++ food-blog/src/assets/index.js
@@ -0,0 +1,85 @@
+import headerBackground576 from "./img/HeaderBackground576.jpg";
+import headerBackground768 from "./img/HeaderBackground768.jpg";
+import headerBackground1024 from "./img/HeaderBackground1024.jpg";
+import headerBackground1360 from "./img/HeaderBackground1360.jpg";
+import headerBackground1920 from "./img/HeaderBackground1920.jpg";
+import gala from "./img/gala.jpg";
+import dasha from "./img/dasha.jpg";
+import mary from "./img/mary.jpg";
+import becomeAuthor576 from "./img/become-author576.jpg";
+import becomeAuthor768 from "./img/become-author768.jpg";
+import becomeAuthor1024 from "./img/become-author1024.jpg";
+import becomeAuthor1360 from "./img/become-author1360.jpg";
+import becomeAuthor1920 from "./img/become-author1920.jpg";
+import post1 from "./img/post-1.jpg";
+import post2 from "./img/post-2.jpg";
+import post3 from "./img/post-3.jpg";
+import post4 from "./img/post-4.jpg";
+import post5 from "./img/post-5.jpg";
+import post6 from "./img/post-6.jpg";
+import post7 from "./img/post-7.jpg";
+import post8 from "./img/post-8.jpg";
+import post9 from "./img/post-9.jpg";
+import post10 from "./img/post-10.jpg";
+import post11 from "./img/post-11.jpg";
+import post12 from "./img/post-12.jpg";
+import post13 from "./img/post-13.jpg";
+import post14 from "./img/post-14.jpg";
+import post15 from "./img/post-15.jpg";
+import post16 from "./img/post-16.jpg";
+import post17 from "./img/post-17.jpg";
+import post18 from "./img/post-18.jpg";
+import featuredOne576 from "./img/featured-one576.jpg";
+import featuredOne768 from "./img/featured-one768.jpg";
+import featuredOne1024 from "./img/featured-one1024.jpg";
+import featuredOne1360 from "./img/featured-one1360.jpg";
+import featuredOne1920 from "./img/featured-one1920.jpg";
+import table576 from "./img/table576.jpg";
+import table768 from "./img/table768.jpg";
+import table1024 from "./img/table1024.jpg";
+import table1360 from "./img/table1360.jpg";
+import table1920 from "./img/table1920.jpg";
+
+export {
+  headerBackground576,
+  headerBackground768,
+  headerBackground1024,
+  headerBackground1360,
+  headerBackground1920,
+  gala,
+  dasha,
+  mary,
+  becomeAuthor576,
+  becomeAuthor768,
+  becomeAuthor1024,
+  becomeAuthor1360,
+  becomeAuthor1920,
+  post1,
+  post2,
+  post3,
+  post4,
+  post5,
+  post6,
+  post7,
+  post8,
+  post9,
+  post10,
+  post11,
+  post12,
+  post13,
+  post14,
+  post15,
+  post16,
+  post17,
+  post18,
+  featuredOne576,
+  featuredOne768,
+  featuredOne1024,
+  featuredOne1360,
+  featuredOne1920,
+  table576,
+  table768,
+  table1024,
+  table1360,
+  table1920,
+};
