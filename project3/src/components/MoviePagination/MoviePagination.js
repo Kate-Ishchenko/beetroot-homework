da5e1a1ca7eb6e73de15cda06562251c2f87d8b5
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:20:19 +0200

    Add files via upload

---
+++ project3/src/components/MoviePagination/MoviePagination.js
@@ -0,0 +1,22 @@
+import { Pagination } from "@mui/material";
+import "./MoviePagination.scss";
+
+const MoviePagination = ({ setPage, numOfPages = 10 }) => {
+  const handlePageChange = (page) => {
+    setPage(page);
+    window.scroll(0, 0);
+  };
+  return (
+    <div className="pagination-container">
+      <Pagination
+        onChange={(e) => handlePageChange(e.target.textContent)}
+        count={numOfPages}
+        color="primary"
+        hideNextButton
+        hidePrevButton
+      />
+    </div>
+  );
+};
+
+export default MoviePagination;
