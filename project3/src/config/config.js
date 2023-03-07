914f9ea3de8f58477beb632678e1be70ca8103de
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:13:17 +0200

    Add files via upload

---
+++ project3/src/config/config.js
@@ -0,0 +1,14 @@
+import {unavailableImg, unavailableLandscapeImg, noPictureImg} from "../assets/index";
+
+//image sizes for tmdb
+export const img_300 = "https://image.tmdb.org/t/p/w300";
+export const img_500 = "https://image.tmdb.org/t/p/w500";
+
+// contentModal and singleContent
+export const unavailable ={unavailableImg};
+
+// contentModal
+export const unavailableLandscape ={ unavailableLandscapeImg};
+
+// For Carousel
+export const noPicture ={noPictureImg};
