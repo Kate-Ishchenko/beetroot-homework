97cb09817bbc58f357d4db357f01b18f1ddc9302
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:06:58 +0200

    Add files via upload

---
+++ project3/src/pages/index.js
@@ -0,0 +1,6 @@
+import MainPage from "./MainPage/MainPage";
+import Movies from "./Movies/Movies";
+import TvSeries from "./TvSeries/TvSeries";
+import Search from "./Search/Search";
+
+export {MainPage, Movies, TvSeries, Search}
