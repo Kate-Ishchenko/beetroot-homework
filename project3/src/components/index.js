c6b188fdf588d9fbd1080ec6a9b8705221485517
Kate-Ishchenko <97528043+Kate-Ishchenko@users.noreply.github.com>
2023-03-07 21:15:59 +0200

    Add files via upload

---
+++ project3/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import MainNav from "./MainNav/MainNav";
+import Trending from "./Trending/Trending";
+import MoviesList from "./MoviesList/MoviesList";
+import TvSeries from "../pages/TvSeries/TvSeries";
+import Search from "@mui/icons-material/Search";
+import SingleMovieCard from "./SingleMovieCard/SingleMovieCard";
+import MoviePagination from "./MoviePagination/MoviePagination";
+import GenresList from "./GenresList/GenresList";
+import TvSeriesList from "./TvSeriesList/TvSeriesList";
+import SearchMovies from "./SearchMovies/SearchMovies";
+
+export {Header, MainNav, Trending, MoviesList, TvSeries, Search, SingleMovieCard, MoviePagination, GenresList, TvSeriesList, SearchMovies};
